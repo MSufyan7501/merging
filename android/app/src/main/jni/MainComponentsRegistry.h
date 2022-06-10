58741bfe9442a156859366d1d603e82fcb421a78
MSufyan7501 <81415490+MSufyan7501@users.noreply.github.com>
2022-06-10 23:05:55 +0500

    Add files via upload

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/nauticalpay/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/nauticalapp/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
